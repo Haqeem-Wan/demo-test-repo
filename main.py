d392850d21dd71656fad80fde6ae74ab5d6209e0
Haqeem Wan <haqeemwan@gmail.com>
2022-07-22 04:09:28 +0800

    Begin Project

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ ppvFile = "ppv.csv"
 peopleInfoList = []
 csvInfoList = []
 
-with open(ppvFile, 'r') as readFile:
+with open(peopleFile, 'r') as readFile:
     csvreader = csv.reader(readFile)
+
+    csvInfoList = next(csvreader)
 
     for row in csvreader:
         peopleInfoList.append(row)
 
     print("Total no. of rows: %d"%(csvreader.line_num))
+
+print('\nFirst 5 rows are:\n')
+for row in peopleInfoList[:10]:
+    # parsing each column of a row
+    for col in row:
+        print("%10s"%col,end=" "),
+    print('\n')
