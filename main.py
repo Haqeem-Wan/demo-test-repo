e5f8341dc49e8181d86057d7307611ea1d0c6e96
Haqeem Wan <haqeemwan@gmail.com>
2022-07-22 04:01:36 +0800

    Test File

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import csv
+
 print("Hello world")
+
+peopleFile = "people.csv"
+ppvFile = "ppv.csv"
+
+peopleInfoList = []
+csvInfoList = []
+
+with open(ppvFile, 'r') as readFile:
+    csvreader = csv.reader(readFile)
+
+    for row in csvreader:
+        peopleInfoList.append(row)
+
+    print("Total no. of rows: %d"%(csvreader.line_num))
