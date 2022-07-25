6b35401b7e0b02e806b577e29ff11397dc8f58e4
Haqeem Wan <haqeemwan@gmail.com>
2022-07-26 00:12:37 +0800

    Second method done

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import csv
 import math
 import time
+import pandas as pd
+from geopy.distance import geodesic
 from fileinput import close
+from pprint import pprint
 
 from numpy import array
 
 peopleFile = "people.csv"
 ppvFile = "ppv.csv"
 
-people_info_list = []
-ppv_info_list = []
+people_lat_list = []
+people_lon_list = []
+ppv_lat_list = []
+ppv_lon_list = []
 distance_compare_list = []
 paired_list = []
 
 def coordinate_distance_formula(lat_1, lon_1, lat_2, lon_2):
-    lat_distance = float(lat_1) - float(lat_2)
-    lon_distance = float(lon_1) - float(lon_2)
+    radius = 6371 # Radius of the Earth
 
-    return math.sqrt(pow(lat_distance, 2) + pow(lon_distance, 2)) * 60
+    distance_lat = math.radians(float(lat_2) - float(lat_1))
+    distance_lon = math.radians(float(lon_2) - float(lon_1))
+    a = math.sin(float(distance_lat)/2) * math.sin(float(distance_lat)/2) + math.cos(math.radians(float(lat_1))) * math.cos(math.radians(float(lat_2))) * math.sin(float(distance_lon)/2) * math.sin(float(distance_lon)/2)
 
-def read_files() :
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    d = radius * c
+
+    return d
+
+def read_files_csvreader() :
     with open(peopleFile, 'r') as readFile:
         csvreader = csv.reader(readFile)
 
         next(csvreader)
 
         for row in csvreader:
-            people_info_list.append(row)
+            people_lat_list.append(row[1])
+            people_lon_list.append(row[2])
 
         print("Total no. of rows of people : %d"%(csvreader.line_num))
 
@@ -36,28 +49,43 @@ def read_files() :
         next(csvreader)
 
         for row in csvreader:
-            ppv_info_list.append(row)
+            ppv_lat_list.append(row[1])
+            ppv_lon_list.append(row[2])
 
         print("Total no. of rows of ppv : %d"%(csvreader.line_num))
 
+def read_files_pandas() :
+    print("filler")
 
-
-# --- Start of Method 1: (Slowest Method) ---
+# --- Start of Method 1: ---
 
 def method_one() :
     start_time = time.time()
 
-    for people_row in people_info_list:
-        for ppv_row in ppv_info_list:
-            distance_test = coordinate_distance_formula(people_row[1], people_row[2], ppv_row[1], ppv_row[2])
+    read_files_csvreader()
+
+    i = 0
+    j = 0
+
+    #while i <  len(people_lat_list):
+    while i <  5:
+        while j < len(ppv_lat_list) :
+            distance_test = coordinate_distance_formula(people_lat_list[i], people_lon_list[i], ppv_lat_list[j], ppv_lon_list[j])
             distance_compare_list.append(distance_test)
-        
+
+            j += 1
+
         closest_distance = min(distance_compare_list)
 
-        closest_ppv = ppv_info_list[distance_compare_list.index(closest_distance)][0]
+        closest_ppv = distance_compare_list.index(closest_distance)
         
-        paired_list.append([people_row[0], closest_ppv, closest_distance])
+        paired_list.append([i, closest_ppv, closest_distance])
         distance_compare_list.clear()
+
+        j = 0
+        i += 1
+
+    pprint(paired_list)
 
     end_time = time.time()
 
@@ -72,9 +100,32 @@ def method_one() :
 def method_two() :
     start_time = time.time()
 
-    people_numpy_array = array(people_info_list)
-    ppv_numpy_array = array(ppv_info_list)
+    read_files_csvreader()
 
+    current_short_distance = 0
+    compare_distance = 0
+    shortest_index = 0
+
+    #for i in range(0, len(people_lat_list), 1) :
+    for i in range(0, 5, 1) :
+        current_person = (people_lat_list[i], people_lon_list[i])
+        for j in range(0, len(ppv_lat_list), 1) :
+            current_ppv = (ppv_lat_list[j], ppv_lon_list[j])
+            compare_distance = geodesic(current_person, current_ppv).km
+            
+            if(compare_distance < current_short_distance and j > 0) :
+                current_short_distance = compare_distance
+                shortest_index = j
+            elif(j == 0) :
+                current_short_distance = compare_distance
+
+        closest_ppv = shortest_index
+        
+        paired_list.append([i, closest_ppv, current_short_distance])
+        distance_compare_list.clear()
+        current_short_distance = 0
+
+    pprint(paired_list)
     end_time = time.time()
 
     print("\nExecution Time (Method 2) : ", (end_time - start_time))
@@ -88,6 +139,7 @@ def method_two() :
 def method_three():
     start_time = time.time()
 
+    read_files_pandas()
 
 
     end_time = time.time()
@@ -98,30 +150,18 @@ def method_three():
 
 # --- Execution Code ---
 
-read_files()
-method_one()
-method_two()
-# method_three()
+method_one()                # Haversine formula + While Loop + csvreader
+paired_list.clear()         
+method_two()                # Geopy + For Loop + csvreader
+paired_list.clear()
+method_three()            # Numpy + Pandas + dataframes
 
+"""people_lat_numpy_array = array(people_lat_list)
+    people_lon_numpy_array = array(people_lon_list)
+    ppv_lat_numpy_array = array(ppv_lat_list)
+    ppv_lon_numpy_array = array(ppv_lon_list)
 
-"""
-    print('\nFirst 5 rows are:\n')
-    for row in people_info_list[:10]:
-        # parsing each column of a row
-        row.append("TESTSPACE")
-        print(row)
-        print('\n')
+    print(people_lat_numpy_array[1])
 
-    for row in people_info_list[:5]:
-        for col in row:
-            print("%10s"%col,end=" "),
-        print('\n')
-"""
-
-"""
-    Calculate Distance with Distance formula : sqrt[(x1-x2)^2 + (y1-y2)^2]
-    Multiply by 60
-        This is already the nautical mile distance
-    Multiply by  1.852
-        This is the kilometer distance
-"""
+    for index in range(0, len(people_lat_numpy_array), 1) :
+        print([math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2) for x0, y0, x1, y1 in zip(people_lat_numpy_array[index], people_lon_numpy_array[index], ppv_lat_numpy_array, ppv_lon_numpy_array)])"""
